5bd92c4cea22a1ba443b18329aaa780ebd6bf642
Ivan Office PC <ibogun2010@my.fit.edu>
2013-05-22 13:11:04 -0400

    added plotting scripts (unnormalized trajectories and velocities profiles)

---
+++ localization/findMostCorrelated.m
@@ -0,0 +1,42 @@
+function [ res ] = findMostCorrelated( d )
+%FINDMOSTCORRELATED Find the most correlated trajectory with object
+%   From the trajectories of the left and right hand (l,r respectively)
+%   find the one which is the most correlated with the trajectory of the
+%   object, o.
+%
+%   Input:
+%
+%   d               -           object of type 'dataEntry'
+%
+%   Output:
+%
+%   res             -           trejectory, either l or r, which is the 
+%       most correlated with o
+%
+%   author: Ivan Bogun
+
+
+% object
+o=d.trajectoryObject;
+o=o.singlePointArray;
+
+% left hand
+l=d.trajectoryLefthand;
+l=l.singlePointArray;
+
+% right hand
+r=d.trajectoryRighthand;
+r=r.singlePointArray;
+
+
+[~,p1]=corr(o(:),l(:));
+[~,p2]=corr(o(:),r(:));
+
+if (p2<p1)
+    res=r;
+else
+    res=l;
+end
+
+end
+
