a803301b11c53795c5c1535ab14a0c6bb970018e
Ivan  Bogun <ibogun2010@host75-61.harris-cc.fit.edu>
2013-06-05 15:50:18 -0400

    finished localization code

--- localization/main.m
+++ localization/main.m
@@ -3,14 +3,36 @@ clc;clear;close all;
 load('fullRawDataSet');
 
 
+over=0;
+under=0;
 
-for i=1:10
-    [f,l]=localizeInteraction(data(i),15);
-    display('calculated');
-    display([f l]);
-    display('real');
-    display([data(i).tInteractionStart, data(i).tInteractionStop]);
+threshold=30;
+
+within=0;
+
+
+for i=1:54
+    [f,l]=localizeInteraction(data(i));
+    
+    start=data(i).tInteractionStart;
+    stop=data(i).tInteractionStop;
+    
+    if (f>stop)
+        over=over+1;
+    end
+    if (l<start)
+        under=under+1;
+    end
+    
+    if (abs(start-f)<threshold && abs(stop-l)<threshold)
+        within=within+1;
+    end
+    
 end
+
+display(over);
+display(under);
+display(within);
 
 
 
